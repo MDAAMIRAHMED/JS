52daa325dc03daa3fd391469bbcd7e83f943c36f
Mohammed Aamir Ahmed <118830391+MDAAMIRAHMED@users.noreply.github.com>
2025-04-26 16:45:49 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOfDatatypes.js
@@ -0,0 +1,31 @@
+let score
+
+// console.log(score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+// console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+
+// "33" => 33
+// "33abc" => NaN
+// "hitesh" => NaN
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = undefined
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 0 => false; any other number => true
+// "" => false; "abc" => true
+// null => false
+// undefined => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof(stringNumber))
