9a2b37d0d81798e3659bbbc964cbcd17991ba096
Mohammed Aamir Ahmed <118830391+MDAAMIRAHMED@users.noreply.github.com>
2025-04-26 10:01:32 +0000

    js datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code with newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// visit "https://tc39.es/ecma262/#sec-white-space" or mdn for documentation
+// code readability should be high
+console.log("hello")
+
+let name = "Aamir"
+let age = 22
+let isLoggedIn = false
+let state;
+console.log(state)
+// number 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof "hello")
+console.log(typeof null)
+console.log(typeof undefined)
